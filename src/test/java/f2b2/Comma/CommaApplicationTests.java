d57d5ca6e8293815d7514a65534edd0687ab6617
kmsy120 <kmsy120@naver.com>
2022-12-13 08:59:06 +0900

    database 루트 수정

--- src/test/java/f2b2/Comma/CommaApplicationTests.java
+++ src/test/java/f2b2/Comma/CommaApplicationTests.java
@@ -2,7 +2,9 @@ package f2b2.Comma;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@WebAppConfiguration
 @SpringBootTest
 class CommaApplicationTests {
 
--- src/test/java/f2b2/Comma/user/UserServiceTest.java
+++ src/test/java/f2b2/Comma/user/UserServiceTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import javax.transaction.Transactional;
 
@@ -16,7 +17,6 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
 class UserServiceTest {
-
 
     @Autowired
     UserService userService;
